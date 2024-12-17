d347d028b48fcf09359d45b42e1cbf87fa116731
Dmitry <d.baydarov@webpractik.ru>
2024-12-17 15:49:44 +0300

    refactor(eslint): замена конфига на flat, обновление esling до 9 версии Refs: KT-39825891

---
+++ index.mjs
@@ -0,0 +1,122 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
+import jsxA11Y from "eslint-plugin-jsx-a11y";
+import _import from "eslint-plugin-import";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import xss from "eslint-plugin-xss";
+import noSecrets from "eslint-plugin-no-secrets";
+import sonarjs from "eslint-plugin-sonarjs";
+import unicorn from "eslint-plugin-unicorn";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+const __dirname = import.meta.dirname;
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...fixupConfigRules(compat.extends(
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:xss/recommended",
+    "plugin:sonarjs/recommended-legacy",
+    "plugin:storybook/recommended",
+    "plugin:unicorn/recommended",
+    "plugin:compat/recommended",
+    "/home/dmitri/projects/eslint-config-webpractik/rules/best-practice.js",
+    "/home/dmitri/projects/eslint-config-webpractik/rules/react.js",
+    "/home/dmitri/projects/eslint-config-webpractik/rules/import.js",
+    "/home/dmitri/projects/eslint-config-webpractik/rules/unicorn.js",
+)), {
+    plugins: {
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        react: fixupPluginRules(react),
+        "react-hooks": fixupPluginRules(reactHooks),
+        prettier,
+        "jsx-a11y": fixupPluginRules(jsxA11Y),
+        import: fixupPluginRules(_import),
+        "simple-import-sort": simpleImportSort,
+        "unused-imports": unusedImports,
+        xss: fixupPluginRules(xss),
+        "no-secrets": noSecrets,
+        sonarjs: fixupPluginRules(sonarjs),
+        compat: fixupPluginRules(compat),
+        unicorn: fixupPluginRules(unicorn),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+            window: true,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts", ".tsx"],
+        },
+
+        "import/resolver": {
+            typescript: {
+                project: "./",
+            },
+        },
+
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+        "@typescript-eslint/no-use-before-define": "error",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+
+        "@typescript-eslint/unbound-method": ["error", {
+            ignoreStatic: true,
+        }],
+
+        "@typescript-eslint/no-unused-vars": "error",
+        "sonarjs/sonar-prefer-read-only-props": "off",
+
+        "xss/no-mixed-html": [2, {
+            functions: {
+                sanitize: {
+                    htmlOutput: true,
+                },
+            },
+        }],
+
+        "no-secrets/no-secrets": "error",
+    },
+}];
