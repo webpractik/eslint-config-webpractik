575b8ba73113458214fa1a1f7cbe7b89a72e6386
Павел Воробьев <styrgia@gmail.com>
2025-01-20 11:04:48 +0300

    feat(publishing): add changesets

---
+++ .changeset/light-melons-brush.md
@@ -0,0 +1,10 @@
+---
+'@wp/eslint-config-legacy': major
+'@wp/eslint-config-react': major
+'@wp/eslint-config-wp': major
+'@wp/eslint-config-next': major
+'@wp/eslint-config-node': major
+'@wp/eslint-config-vue': major
+---
+
+init packages
--- release.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    branches: ['main'],
-    extends: 'semantic-release-monorepo',
-    plugins: [
-        '@semantic-release/changelog',
-        ['@semantic-release/npm', { npmPublish: true }],
-        '@semantic-release/git',
-    ],
-};
