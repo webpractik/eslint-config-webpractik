868a70fbea74c00ca82f50c3c6f284c5f7622911
Павел Воробьев <styrgia@gmail.com>
2025-01-23 23:33:36 +0300

    fix(sonar): function-return-type off

--- packages/base/rules/sonar.js
+++ packages/base/rules/sonar.js
@@ -2,4 +2,5 @@ export default {
     'sonarjs/no-misused-promises': 0,
     'sonarjs/prefer-immediate-return': 0,
     'sonarjs/prefer-read-only-props': 0,
+    'sonarjs/function-return-type': 0,
 };
